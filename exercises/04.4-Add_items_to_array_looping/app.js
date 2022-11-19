22c24c4567ae9bf8cd3cb5ae46fb7bf352560286
john.banks <johndbanksm@gmail.com>
2022-11-19 20:27:41 +0000

    added 4.4

--- exercises/04.4-Add_items_to_array_looping/app.js
+++ exercises/04.4-Add_items_to_array_looping/app.js
@@ -4,3 +4,9 @@ let arr = [4,5,734,43,45];
 // Your code here
 // you need to loop 10 times, for example, using a for loop
 //*****************
+for (let i = 0; i < 10; i++) {
+    const randomInt = Math.floor(Math.random(i) * 10 + 1);
+    let addedInt = arr.push(randomInt);
+}
+
+console.log(arr)
